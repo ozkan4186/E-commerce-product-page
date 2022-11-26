85da4080ecd41e52669111cc7bb60ea1f13cb2e1
özkan demir <ozkanndemir2009@gmail.com>
2022-11-26 22:19:58 +0300

    edit

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu= document.querySelector(".hamb")
+menu.addEventListener("click",(e)=>{
+    if(e.target.claslist.contains("hambclick")){
+        e.target.claslist.remove("hambclick")
+    }
+})
+const artı=document.querySelector()
